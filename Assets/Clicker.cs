6af65ec803a4d6a4169801922a708fe953b12a9b
Avreil <Alabastro@gmail.com>
2018-10-22 16:06:41 +0200

    test

---
+++ Assets/Clicker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour {
+
+
+    public GameManager gm;
+
+
+
+
+
+
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            gm.gold += gm.goldPC;
+        }	
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+
+
+    public Text goldDisp;
+    public Text goldPSDisp;
+    public Text goldPCDisp;
+
+    public float gold=0;
+    public float goldPS=0;
+    public float goldPC=0;
+
+	
+	
+
+
+
+    void Start () {}
+	void Update () {
+        goldDisp.text = "Gold:  " + gold;
+        goldPSDisp.text = "Gold Per Second: " + goldPS;
+        goldPCDisp.text = "Gold Per Click: " + goldPC;
+    
+    
+    }
+
+
+
+
+
+
+}
