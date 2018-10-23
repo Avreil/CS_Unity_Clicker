bc70d84cdfdce92e34c5506a3113de333b94bcf1
Avreil <Alabastro@gmail.com>
2018-10-23 16:26:36 +0200

    First Commit

---
+++ Assets/Scripts/BackBtn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackBtn : MonoBehaviour {
+
+    public Button button;
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
+    public GameObject panel4;
+
+   public void backToVillageView(){
+        panel1.gameObject.SetActive(false);
+        panel2.gameObject.SetActive(false);
+        panel3.gameObject.SetActive(false);
+        panel4.gameObject.SetActive(false);
+        button.gameObject.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/UpgradePanels.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradePanels : MonoBehaviour {
+
+   
+
+    public GameObject Panel;
+    public GameObject panelToClose1;
+    public GameObject panelToClose2;
+    public GameObject panelToClose3;
+    public Button button;
+
+    int counter=1;
+
+    public void ShowPanel(){
+        counter++;
+        if (counter %2 == 1){
+            Panel.gameObject.SetActive(false);
+
+
+        }else{
+            Panel.gameObject.SetActive(true);
+            button.gameObject.SetActive(true);
+            closePreviousPanels();
+            counter=1;
+            
+        }
+    }
+
+
+
+    void closePreviousPanels(){
+        panelToClose1.gameObject.SetActive(false);
+        panelToClose2.gameObject.SetActive(false);
+        panelToClose3.gameObject.SetActive(false);
+
+
+    }
+}
